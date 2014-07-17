f34a235d61990ce360b90ff657f964a4feb0a27d
Lori Hutchek <lhutchek@mcdpartners.com>
2014-07-16 20:37:53 -0400

    getting small set working again

--- js/speedtest.js
+++ js/speedtest.js
@@ -2,7 +2,7 @@
 
 //443
 //178853
-
+//
 //modules
 var fs = require('fs');
 
@@ -11,8 +11,8 @@ var names = '',
     dataSet = '',
     nameCheck = {};
 
-names = fs.readFileSync('js/small/names.txt', 'utf8');//.toString();
-dataSet = fs.readFileSync('js/small/data.txt', 'utf8');//.toString();
+names = fs.readFileSync('js/names.txt', 'utf8');//.toString();
+dataSet = fs.readFileSync('js/data.txt', 'utf8');//.toString();
 
 var split = function (needle, haystack) {
   var i = 0, len = haystack.length, word = '', ar = [], chr = '';
@@ -26,13 +26,14 @@ var split = function (needle, haystack) {
       word += chr;
     }
   }
+  ar.push(word);
   return ar;
 };
 
 
 //sum func
 var dataloaded = function () {
-  var ar = split ('\n', names), //names.split('\n'), //
+  var ar = names.split('\n'), //split ('\n', names), //
       len = ar.length, n = '',
       i = 0, sum = 0, num = '', len_num = 0,
       p = 0, loc = -1, sub = '', name = '', j = 0,  k = 0, numNext = false;
@@ -41,49 +42,51 @@ var dataloaded = function () {
     p = dataSet;
     n = ar[i];
 
+    // console.log('NAME: ', n);
     //double check
     if (nameCheck[n] === undefined) {
       nameCheck[n] = true;
       name = n+',';
-
-      console.log(name);
+      
       loc = p.indexOf(name);
 
-      do {
-        numNext = false;
-        num = '';
-        len_num = loc+20;
-        // sub = p.substring(loc, loc+20);
-        for (k = loc; k < len_num; k++) {
-          c = p.charAt(k);
-          // console.log(c);
-          if (c === ',') {
-            num = p.charAt(k+1);
+      if (loc !== -1) {
+        num = 0;
+        //grab num
+        for (j = 0; j < 400; j++) { //do {
+          len_num = loc+20;
+          // sub = p.substring(loc, loc+20);
+          for (k = loc; k < len_num; k++) {
+            c = p.charAt(k);
+            if (c === ',') {
+              num = +p.charAt(k+1);
+              break;
+            }
+          }
+
+          // num = sub.substring(sub.indexOf(',')+1, sub.indexOf('\n'));
+          // console.log(p.substring(loc - 8, loc + 15));
+          // console.log('NUM: '+num);
+          sum += num;
+          // console.log(sum);
+
+          p = p.substring(k, p.length);
+
+          loc = p.indexOf(name);
+          if (loc === -1) {
+            // console.log(name, j);
             break;
           }
-        }
+          // console.log('----');
+        }// while (loc !== -1);
 
-        // num = sub.substring(sub.indexOf(',')+1, sub.indexOf('\n'));
-        console.log(p.substring(loc - 8, loc + 8));
-        console.log('NUM: '+num);
-        sum += Number(num, 10);
-        console.log(sum);
-
-        p = p.substring(loc+1, p.length);
-
-        loc = p.indexOf(name);
-        // if (loc === -1) {
-        //   // console.log(name, j);
-        //   break;
-        // }
-
-      } while (loc !== -1);
-
-      console.log('----------------------------------------');
+        // console.log('----------------------------------------');
+      }
     }
   }
   // console.log(i);
   console.log(sum);
+  // console.log(sum - 178853);
 
 };
 
